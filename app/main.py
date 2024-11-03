6b71b6b128e46a83964e123a3fd68a704e1bcf1d
Danylo Myroshnychenko <danil010701@icloud.com>
2024-11-03 18:53:56 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,39 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list[Car]) -> float:
+        total_income = 0.0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income = self.calculate_washing_price(car)
+                total_income += income
+                car.clean_mark = self.clean_power  # Update clean_mark
+        return round(total_income, 1)
+
+    def calculate_washing_price(self, car: Car) -> float:
+        if car.clean_mark >= self.clean_power:
+            return 0.0
+
+        cost = (car.comfort_class * (self.clean_power - car.clean_mark) *
+                self.average_rating / self.distance_from_city_center)
+        return round(cost, 1)
+
+    def rate_service(self, rating: float) -> None:
+        total_ratings = self.count_of_ratings + 1
+        self.average_rating = (
+                (self.average_rating * self.count_of_ratings + rating) /
+                total_ratings
+        )
+        self.count_of_ratings = total_ratings
