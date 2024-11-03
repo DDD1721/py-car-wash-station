72287bd95c4a574e9658954a2ad0ef64da9e2ec2
Danylo Myroshnychenko <danil010701@icloud.com>
2024-11-03 19:26:51 +0100

    2

--- app/main.py
+++ app/main.py
@@ -24,8 +24,8 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            car.comfort_class * (self.clean_power - car.clean_mark) *
-            self.average_rating / self.distance_from_city_center,
+            (car.comfort_class * (self.clean_power - car.clean_mark) *
+            self.average_rating) / self.distance_from_city_center,
             1
         )
 
