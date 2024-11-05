2ac1f5145792c0438efb09c07fda4a467bf996cb
Danylo Myroshnychenko <danil010701@icloud.com>
2024-11-05 07:44:50 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Car:
     def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
@@ -6,14 +8,19 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list[Car]) -> float:
+    def serve_cars(self, cars: List[Car]) -> float:
         income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
